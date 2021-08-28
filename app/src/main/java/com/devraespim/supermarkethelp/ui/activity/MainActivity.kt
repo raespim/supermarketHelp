b942f12cba9b0d7ff0947c8f50bece72926fcc8f
Raul Araujo Espim <raulespim.p@gmail.com>
2021-08-28 00:09:04 -0300

    navigation excluded

--- app/src/main/java/com/devraespim/supermarkethelp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/devraespim/supermarkethelp/ui/activity/MainActivity.kt
@@ -2,14 +2,32 @@ package com.devraespim.supermarkethelp.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.devraespim.supermarkethelp.MainApplication
 import com.devraespim.supermarkethelp.R
 import com.devraespim.supermarkethelp.ui.fragment.CategoriesFragment
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+    private val manager = supportFragmentManager
+    private val categoriesFragmentTag = "CategoriesFragment"
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
-        supportFragmentManager.beginTransaction().add(R.id.conteiner_fragment, CategoriesFragment()).commit()
+        manager.beginTransaction().add(R.id.conteiner_fragment, CategoriesFragment(), categoriesFragmentTag).commit()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        try {
+            if (manager.findFragmentByTag(categoriesFragmentTag)?.isVisible!!) {
+                supportActionBar?.title = "Principal - Categorias"
+            }
+        } catch (e: Exception){
+            Log.e(TAG, "onBackPressed() " + e.message)
+        }
     }
 }
--- app/src/main/java/com/devraespim/supermarkethelp/ui/fragment/CategoriesFragment.kt
+++ app/src/main/java/com/devraespim/supermarkethelp/ui/fragment/CategoriesFragment.kt
@@ -16,19 +16,25 @@ import com.devraespim.supermarkethelp.ui.fragment.adapter.CategoriesAdapter
 class CategoriesFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity as MainActivity).title = "Principal - Categorias"
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_categories, container, false)
+
         initializeViews(root)
         return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        (activity as MainActivity).title = "Categorias"
         setupRecyclerView()
     }
 
